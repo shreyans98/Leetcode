bb18c701b19c849e2fdc8d7545ba1c0e03a1230b
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-01 20:41:19 +0530

    Time: 3 ms (63.58%), Space: 57.2 MB (76.37%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.java
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -4,16 +4,20 @@ class Solution {
         int correct_pos = 0;
         int temp = 0;
         
-        for(int i=0;i<nums.length;i++) {
+        for(int i=0;i<nums.length;i++) {//loop to iterate through all values and determine its correct position
             correct_pos = nums[i]-1;
-            
+    //in this loop we will start with swap only for those values which are in between 1 to nums.length() since 1 will be the smallest possible integer and nums.length() + 1 is the worstt case possible integer. Last and condition in the below line will be for scenario where we have to swap more than once for a value.
             while(nums[i] >=1 && nums[i] < nums.length && nums[i] != nums[correct_pos]){
                 temp = nums[i];
                 nums[i] = nums[correct_pos];
                 nums[correct_pos] = temp;
+                
+                //this line will change the correct position since nums[i] has changed
                 correct_pos = nums[i]-1;
             }
         }
+        
+        //in this loop we will iterate through the array again and we will take those index which does not have the same value as index itself and hence this will show the first possible smallest integer
         for(int i=0;i<nums.length;i++) {
             if(i+1 != nums[i])
                 return i+1;
