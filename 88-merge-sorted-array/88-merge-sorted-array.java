927d08a31b83e86c968e2f0467e216d8efe0386c
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-01 14:43:47 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (80.26%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,26 +1,5 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        //Brute force approach
-        // int temp = 0;
-        //shift all the elements of nums2 to nums1 in the position where 0 occurs
-        // for(int i=m;i<nums1.length;i++) {
-        //     nums1[i] = nums2[temp];
-        //     temp++;
-        //     }
-        
-        //sort the complete the nums1 array in non decreasing order
-//         for(int i=0;i<nums1.length-1;i++) {
-//             for(int j=i+1;j<nums1.length;j++) {
-//                 if(nums1[i]>nums1[j]) {
-//                     temp = nums1[i];
-//                     nums1[i] = nums1[j];
-//                     nums1[j] = temp;
-//                 }
-//             }
-            
-//         }
-        
-        //above approach is brute force approach
         
        //second approach
         
