c3a3d4133f55c963e31d5c39cd94458a1cc36fed
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-01 23:59:13 +0530

    Time: 0 ms (100.00%), Space: 43 MB (20.24%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.java
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.java
@@ -1,17 +1,23 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
+        int lo = 0;
+        if(matrix.length == 0) return false;
+        int n = matrix.length; 
+        int m = matrix[0].length; 
+        int hi = (n * m) - 1;
         
-        //brute force approach
-        //Two loops are used to access the individual elements and compare it with the target.
-        boolean result=false;
-        for(int i=0;i<matrix.length;i++) { // Loop to access individual sub-array
-            for(int j=0;j<matrix[0].length;j++) { //loop to access the individual elements
-               if(matrix[i][j] == target)
-                   return true;
+        while(lo <= hi) {
+            int mid = (lo + (hi - lo) / 2);
+            if(matrix[mid/m][mid % m] == target) {
+                return true;
+            }
+            if(matrix[mid/m][mid % m] < target) {
+                lo = mid + 1;
+            }
+            else {
+                hi = mid - 1;
             }
         }
-        
         return false;
-        
     }
 }
