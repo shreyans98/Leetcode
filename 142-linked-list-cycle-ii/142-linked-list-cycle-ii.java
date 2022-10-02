840a7eef9f9bc58066d7375029c75a296baa283f
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-03 01:41:29 +0530

    Time: 3 ms (37.25%), Space: 44.8 MB (67.35%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -11,18 +11,18 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode fast = head;
+        ListNode slow = head; //initialised slow pointer with head
+        ListNode fast = head; //initialised fast pointer with head
         ListNode entry = head;
         
        
         
-        while(fast != null && fast.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
+        while(fast != null && fast.next != null) { //loop traversal for checking if a cycle exists or not
+            fast = fast.next.next; // fast moves by 2 position 
+            slow = slow.next; //slow moves by 1 position
             
-            if(fast == slow) {
-                while(entry != slow) {
+            if(fast == slow) { //if fast & slow are equal then we have cycle present in the list
+                while(entry != slow) { // entry moves 1 position at a time, till it is not equal with slow pointer
                     entry = entry.next;
                     slow = slow.next;
                 }
