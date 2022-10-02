a43eb0278698c42ae3fbeba6090a585a6caced05
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-02 12:53:09 +0530

    Time: 6 ms (80.01%), Space: 96.6 MB (81.72%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -10,16 +10,23 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
+        
+//Approach here is to take two pointers fast and slow.
+
         ListNode fast = head;
         ListNode slow = head;
-        
+        // fast moves twice the speed of slow and hence reaches the end of the list when slow is at the middle of the list.
         while(fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
         }
         
+    //we reverse the list from the middle position where slow is upto the end of the list.
         slow = rev(slow);
+        //fast is initialised with head
         fast = head;
+        
+        //loop to check each element of the reversed second half of the list with the first half of the list. If somewhere elements are not same then it will return false
         while(slow != null) {
             if(slow.val != fast.val)
                 return false;
@@ -29,6 +36,7 @@ class Solution {
         return true;
     }
     
+    //method to reverse the second half of the list
     public ListNode rev(ListNode head) {
         ListNode reverse = null;
         while(head != null) {
