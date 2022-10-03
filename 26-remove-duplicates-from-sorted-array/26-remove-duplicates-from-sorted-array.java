f3cbce6385c187be8dc7c296ca54294853c4b544
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-03 17:16:49 +0530

    Time: 1 ms (100.00%), Space: 48.1 MB (27.91%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -1,15 +1,15 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         
-        int temp = 0;
-        int count = 1;
+        int temp = 0; //variable for maintaining the latest element to be exhanged
+        int count = 1; //variable to maintain the position of newly entered element
         
         for(int i=1;i<nums.length;i++) {
-            if(nums[i] != nums[temp]){
+            if(nums[i] != nums[temp]){ //if not equal, it means unique value hence we enter the element and upgrade count
                 nums[count] = nums[i];
                 count++;
             }  
-                temp++;
+                temp++; //else upgrade the element which is latest and to be exchanged
         }
         return count;
         
