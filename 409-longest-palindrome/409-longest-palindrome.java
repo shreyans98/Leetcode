8504d36a3f3744050786e69403347d87d6596b63
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-05 15:22:38 +0530

    Time: 5 ms (68.18%), Space: 42.3 MB (59.50%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.java
+++ 409-longest-palindrome/409-longest-palindrome.java
@@ -6,17 +6,18 @@ class Solution {
         if(s.length() ==1)
             return 1;
         int len = 0;
+        //Hashset to store the frequency of characters
         HashSet<Character> hset = new HashSet<Character>();
         
         for(int i=0;i<s.length();i++) {
             if(!hset.contains(s.charAt(i)))
                hset.add(s.charAt(i));
-            else {
+            else { //if set contains that character then take out that character and increase length by 2 since number of characters will be even now
                 len += 2;
                 hset.remove(s.charAt(i));
             }
         }
-        
+        //if multiple different characters have only 1 occurence or have been used even number of times then we need to increase count by 1 since only once any of them can be used.(length will increase by 1 in all cases)
         if(!hset.isEmpty())
             len++;
                
