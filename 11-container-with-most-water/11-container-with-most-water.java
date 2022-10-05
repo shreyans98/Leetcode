751ca806dfa650eea53cdff14c1681c973241db5
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-05 21:58:00 +0530

    Time: 14 ms (16.59%), Space: 74 MB (60.93%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -1,14 +1,22 @@
 class Solution {
     public int maxArea(int[] height) {
         
+/*
+Intution behind the approach is to find the area for every index.
+Height of every index will be the minimum of two index from both sides, and width can be maximised in this respect.
+
+When height on the left side is smaller than the height on the right side then we have to move the index on this side as the maximum area for this index is achieved. 
+
+Similarly for the right side we will move to the left side.
+*/        
         int left = 0;
         int right = height.length-1;
-        int finalArea = Integer.MIN_VALUE;
+      //  int finalArea = Integer.MIN_VALUE;
         
         int area = 0;
         
         while(left < right) {
-            area = ((Math.min(height[left], height[right])) * (right-left));
+            area = Math.max(((Math.min(height[left], height[right])) * (right-left)), area);
             
             if(height[left] < height[right])
                 left++;
@@ -19,10 +27,10 @@ class Solution {
                 right--;
             }
             
-            finalArea = Math.max(area, finalArea);
+           // finalArea = Math.max(area, finalArea);
         }
         
-        return finalArea;
+        return area;
         
         
     }
