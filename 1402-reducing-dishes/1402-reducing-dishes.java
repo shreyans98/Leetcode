c4a942d0d95207b3d765652d48ef1d941eade6ab
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-08 20:26:44 +0530

    Time: 3 ms (84.98%), Space: 40.1 MB (98.72%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.java
+++ 1402-reducing-dishes/1402-reducing-dishes.java
@@ -8,28 +8,28 @@ class Solution {
         int p = len-1; //variable to access each character
         int temp = 0;
         int sum = 0;
-        int elements = len-1;
+        int elements = len-1; //number of elements for maximum ratio
         int ratio = 0;
         int i = 1;
         
         
         
-        while(elements>=0) {
+        while(elements>=0) { //we will start with maximum elements and reduce it accordingly
             
-            while(elements >= 0) {
+            while(elements >= 0) { //loop to calculate the sum 
                 temp = p1 * satisfaction[p];
                 sum += temp;
-                p--;
-                p1--;
+                p--; //index of each array element
+                p1--; 
                 elements--;
             }
             
-            p = len -1;
+            p = len -1; //restore p to access the last positive element again
             ratio = Math.max(ratio, sum);
-            sum = 0;
-            p1 = len - i;
+            sum = 0; //sum made 0 for next pass
+            p1 = len - i; //reducing the element in every pass
             elements = p1;
-            i++;
+            i++; //decreasing the elements for sum
         }
         
         return ratio;
