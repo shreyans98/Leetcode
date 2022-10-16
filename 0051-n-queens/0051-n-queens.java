f5317993c8c20a285b8da119dc073385109ffdf1
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-16 17:44:06 +0530

    Time: 3 ms (92.96%), Space: 45.5 MB (47.32%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.java
@@ -0,0 +1,75 @@
+class Solution {
+    public List<List<String>> solveNQueens(int n) {
+        
+        char board[][] = new char[n][n];
+        for(int i=0;i<n;i++) 
+            for(int j=0;j<n;j++) 
+                board[i][j] = '.';
+        
+        List<List<String>> res = new ArrayList<List<String>>();
+        
+        solve(0, board, res);
+        
+        return res;
+
+    }
+    
+    public void solve(int col, char[][] board, List<List<String>> res) {
+        if(col == board.length) {
+            res.add(construct(board));
+            return;
+        }
+        
+        for(int row =0;row<board.length;row++) {
+            if(validate(board, row, col)) {
+                board[row][col] = 'Q';
+                solve(col+1, board, res);
+                board[row][col] = '.';
+            }
+        }
+    }
+    
+    public boolean validate(char[][] board, int row, int col) {
+        int duprow = row;
+        int dupcol = col;
+        
+        while(row>=0 && col >=0){
+            if(board[row][col] == 'Q')
+                return false;
+            row--;
+            col--;
+        }
+        
+        row = duprow;
+        col = dupcol;
+        
+        while(col >=0 && row < board.length) {
+            if(board[row][col] == 'Q')
+                return false;
+            col--;
+            row++;
+        }
+        
+        row = duprow;
+        col = dupcol;
+        
+        while(col >=0){
+            if(board[row][col] == 'Q')
+                return false;
+            col--;
+        }
+        
+        return true;
+    }
+    
+    public static List<String> construct(char[][] board) {
+        List<String> res = new LinkedList<String>();
+        
+        for(int i=0;i<board.length;i++) {
+            String s = new String(board[i]);
+            res.add(s);
+        }
+        
+        return res;
+    }
+}
