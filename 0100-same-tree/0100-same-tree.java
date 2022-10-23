31d4234490a32baf13d4ec49a8d1870c976db5c3
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-23 12:02:08 +0530

    Time: 0 ms (100.00%), Space: 41.3 MB (69.54%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,7 +15,11 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        Queue<TreeNode> queue = new LinkedList<>();
+        
+        /*
+        ---------------------------Recursive Approach------------------------------------------
+        
+        */
         
         if(p==null && q == null)
             return true;
@@ -23,31 +27,46 @@ class Solution {
         if(p==null || q == null)
             return false;
         
-        if(p != null && q != null) {
-            queue.offer(p);
-            queue.offer(q);
-        }
+        if(p.val != q.val)
+            return false;
         
-        while(!queue.isEmpty()) {
-            TreeNode first = queue.poll();
-            TreeNode second = queue.poll();
+        return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+        
+        
+        
+//         Queue<TreeNode> queue = new LinkedList<>();
+        
+//         if(p==null && q == null)
+//             return true;
+        
+//         if(p==null || q == null)
+//             return false;
+        
+//         if(p != null && q != null) {
+//             queue.offer(p);
+//             queue.offer(q);
+//         }
+        
+//         while(!queue.isEmpty()) {
+//             TreeNode first = queue.poll();
+//             TreeNode second = queue.poll();
             
-            if(first == null && second == null)
-                continue;
+//             if(first == null && second == null)
+//                 continue;
             
-            if(first == null || second == null)
-                return false;
+//             if(first == null || second == null)
+//                 return false;
             
-            if(first.val != second.val)
-                return false;
+//             if(first.val != second.val)
+//                 return false;
             
-            queue.offer(first.left);
-            queue.offer(second.left);
-            queue.offer(first.right);
-            queue.offer(second.right);
+//             queue.offer(first.left);
+//             queue.offer(second.left);
+//             queue.offer(first.right);
+//             queue.offer(second.right);
             
-        }
-        return true;
+//         }
+//         return true;
         
     }
 }
