0e7186c2436d23d934f6126c11307b12a8e2b2c6
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-27 02:05:51 +0530

    Time: 30 ms (15.43%), Space: 47.5 MB (56.81%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -4,37 +4,39 @@ class Solution {
         int row = matrix.length;
         int col = matrix[0].length;
         
-        int dp[][] = new int[row][col];
+        int prev[] = new int[col];
         
         
         int maxi = Integer.MAX_VALUE;
         
         for(int j=0;j<col;j++)
-            dp[0][j] = matrix[0][j];
+            prev[j] = matrix[0][j];
         
         for(int i=1;i<row;i++) {
+            int curr[] = new int[col];
             for(int j=0;j<col;j++) {
                 
-                int up = matrix[i][j] + dp[i-1][j];
+                int up = matrix[i][j] + prev[j];
                 int leftDia = matrix[i][j];
                 int rightDia = matrix[i][j];
                 
                 if(j>0)
-                    leftDia += dp[i-1][j-1];
+                    leftDia += prev[j-1];
                 else
                     leftDia += (int) Math.pow(10, 9);
                 
                 if(j<col-1)
-                    rightDia += dp[i-1][j+1];
+                    rightDia += prev[j+1];
                 else
                     rightDia += (int) Math.pow(10, 9);
                 
-                dp[i][j] = Math.min(up, Math.min(leftDia, rightDia));
+                curr[j] = Math.min(up, Math.min(leftDia, rightDia));
             }
+            prev = curr;
         }
         int result = Integer.MAX_VALUE;
         for(int i=0;i<col;i++)
-            result = Math.min(result, dp[row-1][i]);
+            result = Math.min(result, prev[i]);
         
         return result;
     }
