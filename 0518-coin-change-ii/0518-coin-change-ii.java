7f84f44ccfc510d34c974319a30f44a4db8fb7aa
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-29 00:12:13 +0530

    Time: 18 ms (35.73%), Space: 51.2 MB (25.04%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -5,31 +5,52 @@ class Solution {
         
         int dp[][] = new int[len][amount+1];
         
-        for(int ar[] : dp)
-            Arrays.fill(ar, -1);
+        for(int i=0;i<len;i++)
+            dp[i][0] = 0;
         
-        return solve(len-1, amount, coins, dp);
+        for(int j=0;j<=amount;j++) {
+            if(j%coins[0]==0)
+                dp[0][j] = 1;
+            else
+                dp[0][j] = 0;
+        }
+        
+        
+        for(int index=1;index<len;index++) {
+            for(int target=0;target<=amount;target++){
+                int notTake = dp[index-1][target];
+                int take = 0;
+                
+                if(target>=coins[index])
+                    take = dp[index][target-coins[index]];
+                
+                dp[index][target] = take + notTake;
+                
+            }
+        }
+        
+        return dp[len-1][amount];
         
     }
     
-    public int solve(int index, int target, int coins[], int dp[][]) {
+//     public int solve(int index, int target, int coins[], int dp[][]) {
         
-        if(index ==0) {
-            if(target % coins[index] ==0)
-                return 1;
-            else
-                return 0;
-        }
+//         if(index ==0) {
+//             if(target % coins[index] ==0)
+//                 return 1;
+//             else
+//                 return 0;
+//         }
         
-        if(dp[index][target] != -1)
-            return dp[index][target];
+//         if(dp[index][target] != -1)
+//             return dp[index][target];
         
-        int notTake = solve(index-1, target, coins, dp);
-        int take = 0;
+//         int notTake = solve(index-1, target, coins, dp);
+//         int take = 0;
         
-        if(target>=coins[index])
-            take = solve(index, target-coins[index], coins, dp);
+//         if(target>=coins[index])
+//             take = solve(index, target-coins[index], coins, dp);
         
-        return dp[index][target] = take+notTake;
-    }
+//         return dp[index][target] = take+notTake;
+//     }
 }
