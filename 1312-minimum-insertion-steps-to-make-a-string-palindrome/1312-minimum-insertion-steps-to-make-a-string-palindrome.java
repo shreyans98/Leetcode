0999481a703879f7b94864e816e0ef53f94005ac
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-30 02:44:33 +0530

    Time: 61 ms (46.77%), Space: 54.3 MB (56.65%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -6,12 +6,14 @@ class Solution {
         sb.reverse();
         String rev = sb.toString();
         
-        int dp[][] = new int[len+1][len+1];
+       // int dp[][] = new int[len+1][len+1];
+        int prev[] = new int[len+1];
+        int cur[] = new int[len+1];
         
-        for(int i=0;i<=len;i++){
-            dp[0][i] = 0;
-            dp[i][0] = 0;
-        }
+        // for(int i=0;i<=len;i++){
+        //     dp[0][i] = 0;
+        //     dp[i][0] = 0;
+        // }
         
         for(int i=1;i<=len;i++) {
             for(int j=1;j<=len;j++) {
@@ -20,13 +22,17 @@ class Solution {
                 char c2 = rev.charAt(j-1);
                 
                 if(c1==c2)
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    cur[j] = 1 + prev[j-1];
+                   // dp[i][j] = 1 + dp[i-1][j-1];
                 else
-                    dp[i][j] = 0 + Math.max(dp[i][j-1], dp[i-1][j]);
+                    cur[j] = 0 + Math.max(cur[j-1], prev[j]);
+                    //dp[i][j] = 0 + Math.max(dp[i][j-1], dp[i-1][j]);
             }
+            prev = (int[])(cur.clone());
         }
         
-        return len - dp[len][len];
+       // return len - dp[len][len];
+        return len - prev[len];
         
     }
 }
