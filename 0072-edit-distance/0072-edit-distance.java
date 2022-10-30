036b779c2ca9381e94a3cf4c21586b9225b03d31
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-30 13:52:23 +0530

    Time: 13 ms (33.25%), Space: 44.8 MB (56.57%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -4,30 +4,31 @@ class Solution {
         int len1 = word1.length();
         int len2 = word2.length();
         
-        int dp[][] = new int[len1+1][len2+1];
+        int prev[] = new int[len2+1];
+        int curr[] = new int[len2+1];
         
-        for(int i=0;i<=len1;i++)
-            dp[i][0] = i;
         
         for(int j=0;j<=len2;j++)
-            dp[0][j] = j;
+            prev[j] = j;
             
         
         for(int i=1;i<=len1;i++) {
+            curr[0] = i;
             for(int j=1;j<=len2;j++) {
                 
                 char c1 = word1.charAt(i-1);
                 char c2 = word2.charAt(j-1);
                 
                 if(c1 == c2)
-                    dp[i][j] = 0 + dp[i-1][j-1];
+                    curr[j] = 0 + prev[j-1];
                 
                 else
-                    dp[i][j] = Math.min(1 + dp[i][j-1], Math.min((1 + dp[i-1][j]), (1 + dp[i-1][j-1]))); 
+                    curr[j] = Math.min(1 + curr[j-1], Math.min((1 + prev[j]), (1 + prev[j-1]))); 
             }
+            prev = (int[])(curr.clone());
         }
         
-        return dp[len1][len2];
+        return prev[len2];
         
     }
 }
@@ -81,6 +82,11 @@ class Solution {
         
     }
 }
+
+
+
+/////////////////////////////////////////////////////////
+Tabulation Solution
 
 
 */
