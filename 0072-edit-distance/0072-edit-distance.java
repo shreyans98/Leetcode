52b86aa72f149fe8e1fb00362992f19b27d67a62
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-30 13:48:12 +0530

    Time: 6 ms (93.24%), Space: 42.4 MB (91.05%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,5 +1,49 @@
 class Solution {
     public int minDistance(String word1, String word2) {
+        
+        int len1 = word1.length();
+        int len2 = word2.length();
+        
+        int dp[][] = new int[len1+1][len2+1];
+        
+        for(int i=0;i<=len1;i++)
+            dp[i][0] = i;
+        
+        for(int j=0;j<=len2;j++)
+            dp[0][j] = j;
+            
+        
+        for(int i=1;i<=len1;i++) {
+            for(int j=1;j<=len2;j++) {
+                
+                char c1 = word1.charAt(i-1);
+                char c2 = word2.charAt(j-1);
+                
+                if(c1 == c2)
+                    dp[i][j] = 0 + dp[i-1][j-1];
+                
+                else
+                    dp[i][j] = Math.min(1 + dp[i][j-1], Math.min((1 + dp[i-1][j]), (1 + dp[i-1][j-1]))); 
+            }
+        }
+        
+        return dp[len1][len2];
+        
+    }
+}
+
+
+
+
+
+/*Memoization Solution
+
+---------------------------------------------------------------------------------------------------------------------------------
+
+
+
+class Solution {
+    public int minDistance(String word1, String word2) {
         int len1 = word1.length();
         int len2 = word2.length();
         
@@ -37,3 +81,6 @@ class Solution {
         
     }
 }
+
+
+*/
