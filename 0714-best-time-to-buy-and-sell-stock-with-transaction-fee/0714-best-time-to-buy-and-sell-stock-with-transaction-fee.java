542a3a51ed2bec9d5e9e79adbcbd0d4d010123c4
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-30 23:49:22 +0530

    Time: 118 ms (6.58%), Space: 80.2 MB (24.32%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -2,6 +2,34 @@ class Solution {
     public int maxProfit(int[] prices, int fee) {
         
         int len = prices.length;
+        int ahead[] = new int[2];
+        int curr[] = new int[2];
+        
+        for(int ind=len-1;ind>=0;ind--){
+            for(int buy=0;buy<=1;buy++) {
+                
+                if(buy ==0)
+                    curr[buy] = Math.max(0 + ahead[0], -prices[ind]+ahead[1]);
+                
+                if(buy ==1)
+                    curr[buy] = Math.max(0 + ahead[1], prices[ind]-fee+ahead[0]);
+                
+            }
+            ahead = (int[])(curr.clone());
+        }
+        
+        return curr[0];
+        
+//         for(int ar[] : dp)
+//             Arrays.fill(ar, -1);
+        
+//         return solve(0, len, fee, 0, prices, dp);
+        
+    }
+    
+    /*
+    
+     int len = prices.length;
         int dp[][] = new int[len+1][2];
         
         for(int ind=len-1;ind>=0;ind--){
@@ -17,14 +45,9 @@ class Solution {
         }
         
         return dp[0][0];
-        
-//         for(int ar[] : dp)
-//             Arrays.fill(ar, -1);
-        
-//         return solve(0, len, fee, 0, prices, dp);
-        
-    }
     
+    
+    */
     public int solve(int ind, int len, int fee, int buy, int prices[], int dp[][]) {
         
         if(ind>=len)
