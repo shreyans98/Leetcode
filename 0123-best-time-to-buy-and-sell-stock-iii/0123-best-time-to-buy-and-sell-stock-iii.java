c4e23833247f9544c3cf2e814e4ef6fff0c3eecb
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-30 21:14:22 +0530

    Time: 243 ms (22.18%), Space: 125.5 MB (19.80%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.java
@@ -6,11 +6,45 @@ class Solution {
         
         int dp[][][] = new int[len+1][2][3];
         
-        for(int ar[][] : dp)
-            for(int arr[] : ar)
-                Arrays.fill(arr, -1);
+        //base case for tabulation
         
-        return solve(0, len, prices, buy, 2, dp);
+//         for(buy = 0;buy<=1;buy++) {
+//             for(int j = 0;j<=2;j++){
+//                 dp[0][buy][j] = 0;
+//             }
+//         }
+        
+//         for(int i=0;i<=len;i++){
+//             for(int k=0;k<=1;k++){
+//                 dp[i][k][0] = 0;
+//             }
+//         }
+        
+        
+        for(int i=len-1;i>=0;i--){
+            for(int buys=0;buys<=1;buys++){
+                for(int cap=1;cap<=2;cap++) {
+                    
+                    if(buys == 0)
+                        dp[i][buys][cap] = Math.max(-prices[i] + dp[i+1][1][cap], 0 + dp[i+1][0][cap]);
+                    
+                    if(buys ==1)
+                        dp[i][buys][cap] = Math.max(prices[i] + dp[i+1][0][cap-1], 0 + dp[i+1][1][cap]);
+                }
+            }
+        }
+        return dp[0][0][2];
+        
+        
+        
+        
+        
+        
+//         for(int ar[][] : dp)
+//             for(int arr[] : ar)
+//                 Arrays.fill(arr, -1);
+        
+//         return solve(0, len, prices, buy, 2, dp);
         
     }
     
