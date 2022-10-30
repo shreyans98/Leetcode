5dddcc70ed359873ce9eee86a95e5744345e5e0d
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-10-30 12:16:06 +0530

    Time: 34 ms (56.09%), Space: 50.2 MB (44.06%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -6,10 +6,37 @@ class Solution {
         
         int dp[][] = new int [len1+1][len2+1];
         
-        for(int ar[] : dp)
-            Arrays.fill(ar, -1);
+        //1 based indexing with indexes shifted by +1
         
-        return solve(len1-1, len2-1, s, t, dp);
+        //string2 is exhausted so we will return 1 from here
+        for(int i=0;i<=len1;i++)
+            dp[i][0] = 1;
+        
+        for(int j=1;j<=len2;j++)
+            dp[0][j] = 0;
+        
+        for(int i=1;i<=len1;i++) {
+            for(int j=1;j<=len2;j++) {
+                
+                char c1 = s.charAt(i-1);
+                char c2 = t.charAt(j-1);
+                
+                if(c1 == c2)
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        
+        return dp[len1][len2];
+        
+        
+        
+//         for(int ar[] : dp)
+//             Arrays.fill(ar, -1);
+        
+//         return solve(len1-1, len2-1, s, t, dp);
         
     }
     
