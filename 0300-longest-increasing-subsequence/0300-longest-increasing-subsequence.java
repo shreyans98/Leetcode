42bc47712f6e0de930a8be70fa75b6e4c9eae07e
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-11-03 00:02:02 +0530

    Time: 140 ms (15.52%), Space: 44.4 MB (45.14%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -2,21 +2,23 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         
         int len = nums.length;
-        int dp[][] = new int[len+1][len+1];
+        int next[] = new int[len+1];
+        int curr[] = new int[len+1];
         
         for(int ind=len-1;ind>=0;ind--) {
             for(int prev_ind = ind-1;prev_ind>=-1;prev_ind--){
                 
-                int notTake = 0 + dp[ind+1][prev_ind+1];
+                int notTake = 0 + next[prev_ind+1];
                 int take = 0;
                 if(prev_ind == -1 || nums[ind]>nums[prev_ind])
-                    take = 1 + dp[ind+1][ind+1];
+                    take = 1 + next[ind+1];
                 
-                dp[ind][prev_ind+1] = Math.max(take, notTake);
+                curr[prev_ind+1] = Math.max(take, notTake);
             }
+            next = curr;
         }
         
-        return dp[0][-1+1];
+        return next[-1+1];
         
 //         for(int ar[] : dp)
 //             Arrays.fill(ar, -1);
@@ -45,4 +47,27 @@ class Solution {
         
         return dp[ind][prev_ind+1] = Math.max(take, notTake);
     }
+    
+    /*
+    
+    int len = nums.length;
+        int dp[][] = new int[len+1][len+1];
+        
+        for(int ind=len-1;ind>=0;ind--) {
+            for(int prev_ind = ind-1;prev_ind>=-1;prev_ind--){
+                
+                int notTake = 0 + dp[ind+1][prev_ind+1];
+                int take = 0;
+                if(prev_ind == -1 || nums[ind]>nums[prev_ind])
+                    take = 1 + dp[ind+1][ind+1];
+                
+                dp[ind][prev_ind+1] = Math.max(take, notTake);
+            }
+        }
+        
+        return dp[0][-1+1];
+    
+    
+    
+    */
 }
