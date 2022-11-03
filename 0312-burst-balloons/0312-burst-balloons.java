a1b16357caa219eacf692797bcc9fc9ebac3069a
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-11-04 02:35:43 +0530

    Time: 220 ms (37.37%), Space: 43.7 MB (16.61%) - LeetHub

---
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int maxCoins(int[] nums) {
+        
+        int temp[] = new int[nums.length+2];
+        temp[0] = 1;
+        
+        for(int i=1;i<temp.length-1;i++)
+            temp[i] = nums[i-1];
+        temp[temp.length-1] = 1;
+        
+        int dp[][] = new int[temp.length+2][temp.length+2];
+        
+        for(int ar[] : dp)
+            Arrays.fill(ar, -1);
+        
+        
+        return solve(1, nums.length, temp, dp);
+        
+    }
+    
+    public int solve(int i, int j, int temp[], int dp[][]) {
+        //base case
+        if(i>j)
+            return 0;
+        
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        
+        int mini = Integer.MIN_VALUE;
+        for(int ind = i;ind<=j;ind++){
+            int cost = temp[i-1]*temp[ind]*temp[j+1] + solve(i, ind-1, temp, dp) + solve(ind+1, j, temp, dp);
+            mini = Math.max(mini, cost);
+        }
+        
+        return dp[i][j] = mini;
+    }
+}
