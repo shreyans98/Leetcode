475580ab295fe39f7a68ef8c656bf93d0f765e49
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-11-04 02:45:57 +0530

    Time: 39 ms (98.95%), Space: 41.7 MB (94.56%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -10,11 +10,29 @@ class Solution {
         
         int dp[][] = new int[temp.length+2][temp.length+2];
         
-        for(int ar[] : dp)
-            Arrays.fill(ar, -1);
+        
+        for(int i = nums.length;i>=1;i--) {
+            for(int j=1;j<=nums.length;j++) {
+                if(i>j)
+                    continue;
+                
+                int mini = Integer.MIN_VALUE;
+                for(int ind = i;ind<=j;ind++) {
+                    int cost = temp[i-1]*temp[ind]*temp[j+1] + dp[i][ind-1] + dp[ind+1][j];
+                    mini = Math.max(mini, cost);
+                }
+                dp[i][j] = mini;
+            }
+        }
         
         
-        return solve(1, nums.length, temp, dp);
+        return dp[1][nums.length];
+        
+//         for(int ar[] : dp)
+//             Arrays.fill(ar, -1);
+        
+        
+//         return solve(1, nums.length, temp, dp);
         
     }
     
