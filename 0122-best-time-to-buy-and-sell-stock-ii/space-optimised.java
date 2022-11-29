e0949853e75552b2af8904ac517c28010aec9607
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-11-29 12:43:30 +0530

    space optimised

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/space-optimised.java
@@ -0,0 +1,64 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int len = prices.length;
+        //int buy = 0;
+        
+        //int dp[][] = new int[len+1][2];
+        int ahead[] = new int[2];
+        ahead[0] = ahead[1] = 0;
+        
+        int curr[] = new int[2];
+       // dp[len][0] = dp[len][1] = 0;
+        
+        for(int ind=len-1;ind>=0;ind--) {
+            for(int buy = 0;buy<=1;buy++ ) {
+                int profit = 0;
+                
+                if(buy == 0)  
+                    profit = Math.max(-prices[ind] + ahead[1], 0 + ahead[0] );
+                   // profit = Math.max(-prices[ind] + dp[ind+1][1],  0 + dp[ind+1][0]);
+                
+                if(buy == 1)
+                    profit = Math.max(prices[ind]+ahead[0], 0+ahead[1]);
+                   // profit = Math.max( prices[ind] + dp[ind+1][0], 0 + dp[ind+1][1]);  
+              
+                curr[buy] = profit;
+
+            }
+            ahead = curr.clone();
+        }
+        
+        return ahead[0];
+        
+        
+//         for(int ar[] : dp)
+//             Arrays.fill(ar, -1);
+        
+        
+//         return solve(0, len, prices, buy, dp);
+    }
+    
+    public int solve(int ind, int len, int prices[], int buy, int dp[][]) {
+        if(ind == len)
+            return 0;
+        
+        
+        if(dp[ind][buy] != -1)
+            return dp[ind][buy];
+        
+        int profit = 0;
+        
+        
+        
+        if(buy == 0)  
+            profit = Math.max(-prices[ind] + solve(ind+1, len, prices, 1, dp), 0 + solve(ind+1, len, prices, 0, dp));
+        
+        
+        else {
+            profit = Math.max( prices[ind] + solve(ind+1, len, prices, 0, dp), 0 + solve(ind+1, len, prices, 1, dp));
+        }
+        
+        return dp[ind][buy] = profit;
+    }
+}
