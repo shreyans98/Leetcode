6d5f0ed42cd51cdd01e0917f444d7863ab85226a
Shreyans Singhi <shreyanssinghi11@gmail.com>
2022-12-09 12:29:08 +0530

    Space optimised approach

---
+++ 0070-climbing-stairs/0070-climbing-stairs(2).java
@@ -0,0 +1,19 @@
+class Solution {
+    public int climbStairs(int n) {
+        
+        int prev2 = 1;
+        int prev = 1;
+        
+        if(n==0 || n==1)
+            return 1;
+        
+        for(int i=2;i<=n;i++){
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        
+        return prev;
+        
+    }
+}
